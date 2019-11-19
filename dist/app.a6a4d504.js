9988bc74bb71a21e0e2d8bbade49b1acbe703015
zed <chanzedcz@gmail.com>
2019-11-19 19:53:55 +0800

    验证iconPosition为left 或 right 中的一个

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12424,7 +12424,16 @@ exports.default = void 0;
 //
 //
 var _default = {
-  props: ["icon", "iconPosition"]
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function validator(value) {
+        return value === 'left' || value === 'right';
+      }
+    }
+  }
 };
 exports.default = _default;
         var $14084f = exports.default || module.exports;
