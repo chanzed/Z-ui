7183eb4392742cd3b748eba4bb0ee29550bcad63
zed <chanzedcz@gmail.com>
2019-11-24 22:21:30 +0800

    默认为手机样式，删除为Phone写的样式

--- src/components/col.vue
+++ src/components/col.vue
@@ -24,7 +24,6 @@ export default {
     offset: {
       type: [Number, String]
     },
-    phone: { type: Object, validator },
     ipad: { type: Object, validator },
     narrowPc: { type: Object, validator },
     pc: { type: Object, validator },
@@ -43,11 +42,10 @@ export default {
       };
     },
     colClass() {
-      let { span, offset, phone, ipad, narrowPc, pc, widePc } = this;
+      let { span, offset, ipad, narrowPc, pc, widePc } = this;
       return [
         span && `col-${span}`,
         offset && `offset-${offset}`,
-        phone && `col-phone-${phone.span}`,
         ipad && `col-ipad-${ipad.span}`,
         narrowPc && `col-narrow-pc-${narrowPc.span}`,
         pc && `col-pc-${pc.span}`,
@@ -75,22 +73,6 @@ export default {
     }
   }
 
-  @media (max-width: 576px) {
-    $class-prefix: col-phone-;
-
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        width: ($n / 24) * 100%;
-      }
-    }
-    $class-prefix: offset-phone-;
-
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        margin-left: ($n / 24) * 100%;
-      }
-    }
-  }
   @media (min-width: 577px) and (max-width: 768px) {
     $class-prefix: col-ipad-;
 
