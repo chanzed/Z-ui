53ccb29b9579a8dbadffdc2be78516dc2bded0a1
zed <chanzedcz@gmail.com>
2019-11-28 14:47:54 +0800

    支持两种事件触发

--- src/components/popover.vue
+++ src/components/popover.vue
@@ -23,12 +23,51 @@ export default {
       validator(value) {
         return ["top", "bottom", "left", "right"].indexOf(value) !== -1;
       }
+    },
+    trigger: {
+      type: String,
+      default: 'click',
+      validator(value) {
+        return ['click', 'hover'].indexOf(value) !== -1
+      }
     }
   },
   data() {
     return {
       visible: false
     };
+  },
+  mounted() {
+    if (this.trigger === 'click') {
+      this.$refs.popover.addEventListener('click', this.onClick)
+    } else {
+      this.$refs.popover.addEventListener('mouseenter', this.open)
+      this.$refs.popover.addEventListener('mouseleave', this.close)
+    }
+  },
+  destroyed() {
+    if (this.trigger === 'click') {
+      this.$refs.popover.removeEventListener('click', this.onClick)
+    } else {
+      this.$refs.popover.removeEventListener('mouseenter', this.open)
+      this.$refs.popover.removeEventListener('mouseleave', this.close)
+    }
+  },
+  computed: {
+    openEvent() {
+      if (this.trigger === 'click') {
+        return 'click'
+      } else {
+        return 'mouseenter'
+      }
+    },
+    closeEvent() {
+      if (this.trigger === 'click') {
+        return 'click'
+      } else {
+        return 'mouseleave'
+      }
+    }
   },
   methods: {
     positionContent() {
